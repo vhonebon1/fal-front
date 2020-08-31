92ec63ff469a6ac09a0e0279313ee7ee5cbaa2ae
Verity Honebon <vhonebon@gmail.com>
2020-08-31 19:52:17 +0100

    add brand logos

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Linkify from 'react-linkify'
+import logo from '../logo.png'
 
 const AboutPage = ({ cohort }) =>
   <div className="cohort__info-block">
@@ -7,6 +8,7 @@ const AboutPage = ({ cohort }) =>
     <Linkify>
       <div>{cohort.info.split('\n').map( (it, i) => <p key={'x'+i}>{it}</p> )}</div>
     </Linkify>
+    <img class="logo" src={logo} />
   </div>
 
 export default AboutPage
